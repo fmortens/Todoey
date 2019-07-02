64e595eb1e7ed102ee174efa9489c51832e449bd
Frank Mortensen <fmortens@me.com>
2019-07-02 15:45:22 +0200

    Prepare app for category view

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Frank Mortensen on 02/07/2019.
+//  Copyright © 2019 Frank Mortensen. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: -- TableView Datasource Methods
+    
+    // MARK: -- Data Manipulation Methods
+    
+    // MARK: -- Actions
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    
+    // MARK: -- TableView Delegate Methods
+    
+    
+    
+}
