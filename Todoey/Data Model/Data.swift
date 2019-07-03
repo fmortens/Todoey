09a33c965b67e480eb4e5976db948e3cb7d498cd
Frank Mortensen <fmortens@me.com>
2019-07-03 13:42:01 +0200

    Downgrade project to older Xcode

---
+++ Todoey/Data Model/Data.swift
@@ -0,0 +1,17 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by Frank Mortensen on 03/07/2019.
+//  Copyright © 2019 Frank Mortensen. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    
+    var name: String = ""
+    var age: Int = 0
+    
+}
