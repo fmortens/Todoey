0763eef6530b1c83d0d75dce5bc7139f6fba5638
Frank Mortensen <fmortens@me.com>
2019-07-04 12:57:16 +0200

    Add SwipeCellKit to project and list views

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class CategoryViewController: UITableViewController {
+class CategoryViewController: SwipeTableViewController {
     
     let realm = try! Realm()
     var categories: Results<Category>?
@@ -27,7 +27,8 @@ class CategoryViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No categories added yet"
         
         return cell
@@ -65,6 +66,22 @@ class CategoryViewController: UITableViewController {
         }
         
         tableView.reloadData()
+    }
+    
+    
+    // MARK: -- Delete Data from Swipe
+    override func updateModel(at indexPath: IndexPath) {
+    
+        if let selectedCategory = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(selectedCategory)
+                }
+            }  catch {
+                print("Error deleting category \(error)")
+            }
+        }
+        
     }
     
     
--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: SwipeTableViewController {
     
     let realm = try! Realm()
     var items: Results<Item>?
@@ -29,6 +29,21 @@ class TodoListViewController: UITableViewController {
         )
         
         tableView.reloadData()
+    }
+    
+    // MARK: -- Delete Data from Swipe
+    override func updateModel(at indexPath: IndexPath) {
+        
+        if let selectedItem = self.items?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(selectedItem)
+                }
+            }  catch {
+                print("Error deleting category \(error)")
+            }
+        }
+        
     }
     
     
@@ -82,7 +97,7 @@ extension TodoListViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if let item = items?[indexPath.row] {
             cell.textLabel?.text = item.title
@@ -137,4 +152,3 @@ extension TodoListViewController: UISearchBarDelegate {
     }
 
 }
-
